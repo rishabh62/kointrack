17b0981afaeeb612c8ba5382f1e56c03995ab5c3
Rishabh Gour <rishabhgour62@gmail.com>
2017-12-12 17:32:51 +0530

    initial commit

---
+++ controller/App.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
+    "use strict";
+    return Controller.extend("kointrack.controller.App", {
+        onInit: function () {
+
+
+        }
+    });
+
+
+});
